9347fa3e6acc3598d6cdafbe7f486fea6bcb7753
Netza López <l.lopeznetza@gmail.com>
2020-07-09 23:52:14 -0500

    configuración del proyecto
    agregando las vistas, pug y rutas del proyecto

--- server/index.js
+++ server/index.js
@@ -1,9 +1,16 @@
 // Importar express
 const express = require('express');
+const path= require('path');
 const routes = require('./routes');
 
 //Configurar express
 const app = express();
+
+//Habilitar pug
+app.set('view engine', 'pug');
+
+//Añadir las vistas
+app.set('views',path.join(__dirname,'./views'));
 
 //Cargar las rutas
 app.use('/', routes());
