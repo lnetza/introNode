831a51ae503b588cf98753492e34c927f4de32ac
Netza López <l.lopeznetza@gmail.com>
2020-07-14 20:53:04 -0500

    se retira operatorsAliases

--- server/config/database.js
+++ server/config/database.js
@@ -37,7 +37,7 @@ module.exports = new Sequelize(
                     min: 0,
                     acquire: 30000,
                     idle: 10000
-            },
-    operatorsAliases: false
+            }/*,
+    operatorsAliases: false*/
 
 });
